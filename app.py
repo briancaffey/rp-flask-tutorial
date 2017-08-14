bdeb2a022e0fb565592eb177baf3afcc9c561b0b
Brian Caffey <briancaffey2010@gmail.com>
2017-08-14 17:13:16 -0400

    added autoenv and config.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -9,6 +11,8 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+print(os.environ['APP_SETTINGS'])
 
 if __name__ == '__main__':
     app.run()
